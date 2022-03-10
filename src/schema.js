ec4c033b40089fc874d5c4001ccf19a6e63e1f64
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-03-10 17:03:14 +0100

    Track monthly and total views in model

--- src/schema.js
+++ src/schema.js
@@ -5,10 +5,50 @@ mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 const hikaruUserSchema = new mongoose.Schema({
   discordId: { type: String, required: true },
-  watched: { type: [Number] },
-});
-const HikaruUser = mongoose.model('HikaruUser', hikaruUserSchema);
+  watched: { type: [Number], default: [] },
+  total: { type: Number, default: 0 },
+}).post('save', async (doc) => {
+  if (doc.total !== doc.watched.length) {
+    /**
+     * Update Monthly watch count
+     */
+    const date = new Date();
+    const month = 12 * date.getFullYear() + date.getMonth();
+    let monthlyViews = await MonthlyViews.findOne({
+      discordId: doc.discordId,
+      month,
+    });
+    if (!monthlyViews) {
+      monthlyViews = await MonthlyViews.create({
+        discordId: doc.discordId,
+        month,
+      });
+    }
+    monthlyViews.watched += doc.watched.length - doc.total;
+    console.log('monthly views', monthlyViews);
+    monthlyViews.save();
 
-module.exports = { HikaruUser };
+    /**
+     * Update total
+     */
+    doc.total = doc.watched.length;
+    console.log(doc.total);
+    doc.save();
+  }
+});
+
+const monthlyViewsSchema = new mongoose.Schema({
+  discordId: { type: String },
+  // we use integer values for the month, starting from January 1AD
+  // for example march 2022 = (12 * 2022 + 3)
+  month: { type: Number },
+  watched: { type: Number, default: 0 },
+});
+
+const HikaruUser = mongoose.model('HikaruUser', hikaruUserSchema);
+const MonthlyViews = mongoose.model('MonthlyViews', monthlyViewsSchema);
+
+module.exports = { HikaruUser, MonthlyViews };
