54de7643907c9b312c2dbea64367a1a28e6eb9ce
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-03-11 17:02:55 +0100

    Adjust /progress for new schema

--- src/commands/progress.js
+++ src/commands/progress.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { HikaruUser } = require('../schema');
+const { VideoView } = require('../schema');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -9,27 +9,15 @@ module.exports = {
       option.setName('user').setDescription('User').setRequired(false)
     ),
   async execute(interaction) {
-    // determine if we should get progress for interaction user or user provided in args
-    let userQuery = interaction.options.getUser('user');
-    if (!userQuery) {
-      userQuery = interaction.user;
-    }
+    const user = interaction.options.getUser('user') || interaction.user;
+    const discordId = user.id;
 
-    // try to find user
-    const user = await HikaruUser.findOne({
-      discordId: userQuery.id,
-    }).exec();
-    if (!user) {
-      await interaction.reply(
-        `It seems like ${userQuery.username}#${userQuery.discriminator} hasn't logged any videos.`
-      );
-    }
+    const videos = await VideoView.find({ discordId });
 
-    // get number of videos watched
-    const videosWatched = user.watched.length;
+    const videosWatched = videos.length;
 
     await interaction.reply(
-      `${interaction.user.username}#${interaction.user.discriminator} has watched ${videosWatched} videos.`
+      `${user.username}#${user.discriminator} has logged ${videosWatched} videos`
     );
   },
 };
